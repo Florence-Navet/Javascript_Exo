1412076c2a336aff53a337e67cffe3635c2ade86
Florence N <enolaworld@gmail.com>
2025-08-09 16:47:12 +0200

    feat: exercice concatenation

---
+++ src/App/app.js
@@ -0,0 +1,36 @@
+export function substract(a, b) {
+  return a - b;
+}
+
+console.log("total: ", substract(5, 2));
+
+export let nombre = 42;
+
+export let a = 1;
+export let b = "test";
+export let c = [1, 2, 3];
+
+export let a1 = true;
+export let b1 = 12;
+export let c1 = 10n ** 11n;
+export let d1 = "Je m'appelle chouquette";
+
+// let mots = phrase.split(" ");
+// export let c = mots[0] + " " + mots[1];
+// export let d = " " + mots[2];
+// export let e = " " + mots[3] + " " + mots[4] + " !";
+
+//deuxieme methode
+// export let c = phrase.substring(0, 15);
+// export let d = phrase.substring(15, 26);
+// export let e = phrase.substring(26);
+
+// export let phrase = "Essaye de concatener plusieurs chaines !";
+
+// export let decoupe = phrase.split(" ");
+
+// export let c = decoupe.slice(0, 2).join(" ");
+// export let d = decoupe.slice(2, 4).join(" ");
+// export let e = decoupe.slice(4).join(" ");
+
+// export let concatenation = `${c} ${d} ${e}`;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-//index.js
+export let a = 42;
+export let b = 22;
+export let result = a - b;
 
-export function substract(a, b) {
-  return a - b;
-}
+export let phrase = "Essaye de concatener plusieurs chaines !";
+let mots = phrase.split(" ");
+export let c = mots[0] + " " + mots[1];
+export let d = " " + mots[2];
+export let e = " " + mots[3] + " " + mots[4] + " !";
 
-// const result = substract(42, 24);
-// console.log(result);
+export let concatenation = c + d + e;
+console.log(concatenation);
